eeb81334b5d5a0a15bc6462750e415180d24324b
af__n <s.a.jahan.buetcse.394@gmail.com>
2024-12-22 18:40:46 +0600

    "Add ncncnv.c - initial implementation"

---
+++ ncncnv.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    cout<<"hello all"<<endl;
+  }
+    return 0;
+}
